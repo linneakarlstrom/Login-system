1f368685d4a15409758a16030fe573069c2296c9
Linnea Karlström <linnea.karlstrom@skola.botkyrka.se>
2019-11-25 11:32:55 +0100

    You login and when you're in you will be progressed to a quiz

--- login_system.py
+++ login_system.py
@@ -1,25 +1,42 @@
 import quiz
-login = True
+
+users = [
+    
+]
+
+def login(username, password):
+    return username == "ICYLGR01" and password == "Cocoban123"
+    # om username är ICYLGR01 och lösenordet är Cocoban123 returnera True, annars returnera False
+
+login_in = True
 while login:
     username = input("Enter your username:")
-    if username == "ICYLGR01":
-        password = input("Enter your password:")
-        if password == "Cocoban123":
-            print("Welcome", username, ", You may now progress to the quiz \n")
-            quiz.start()
-            
-        if password != "Cocoban123":
-            password_again = input("Incorrect password. Would you like to try to login again? Yes or no?")
-            if password_again.lower() == "yes":
-                login = True
-            if password_again.lower() == "no": 
-                quit()
-            
+    password = input("Enter your password:")
+    if login(username, password):
+         print("Welcome", username, ", You may now progress to the quiz \n") # du lyckades
+         quiz.start()  
     else:
-        username_again = input("Username incorrect. Would you like to try again? Yes or no?")
-        if username_again.lower() == "yes":
-            login = True
-        if username_again.lower() == "no":
-            print("Thank you for trying, welcome back later!")
-            quit()                                                                                                                      
+        print("Incorrect")
+        login_in = True
+        # startas om
+
+    # if username == "ICYLGR01":
+    #     password = input("Enter your password:")
+    #     if password == "Cocoban123":
+    #         print("Welcome", username, ", You may now progress to the quiz \n")
+            
+    #     if password != "Cocoban123":
+    #         password_again = input("Incorrect password. Would you like to try to login again? Yes or no?")
+    #         if password_again.lower() == "yes":
+    #             login = True
+    #         if password_again.lower() == "no": 
+    #             quit()
+            
+    # else:
+    #     username_again = input("Username incorrect. Would you like to try again? Yes or no?")
+    #     if username_again.lower() == "yes":
+    #         login = True
+    #     if username_again.lower() == "no":
+    #         print("Thank you for trying, welcome back later!")
+    #         quit()                                                                                                                      
 
