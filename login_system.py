209ab0d9f6e2e3c1b956ad6802176c176a7b9d2e
Linnea Karlström <linnea.karlstrom@skola.botkyrka.se>
2019-11-28 11:50:19 +0100

    work in progress

--- login_system.py
+++ login_system.py
@@ -1,42 +1,30 @@
 import quiz
 
 users = [
-    
+    {
+        "username": "ICYLGR01",
+        "password": "Cocoban123", 
+    }
 ]
 
+# def register(username,password):
+#     username = input("Username:")
+#     password = input("Password:")
+#     return # de nya keyvalue paren läggs in i users
+
 def login(username, password):
-    return username == "ICYLGR01" and password == "Cocoban123"
+    return username == "ICYLGR01" and password == "Cocoban123"   # det ska läggas in från dictionaryn och sedan kunna användas
     # om username är ICYLGR01 och lösenordet är Cocoban123 returnera True, annars returnera False
 
 login_in = True
-while login:
+while login_in:
     username = input("Enter your username:")
     password = input("Enter your password:")
     if login(username, password):
          print("Welcome", username, ", You may now progress to the quiz \n") # du lyckades
          quiz.start()  
+         # quizet startas
     else:
         print("Incorrect")
         login_in = True
         # startas om
-
-    # if username == "ICYLGR01":
-    #     password = input("Enter your password:")
-    #     if password == "Cocoban123":
-    #         print("Welcome", username, ", You may now progress to the quiz \n")
-            
-    #     if password != "Cocoban123":
-    #         password_again = input("Incorrect password. Would you like to try to login again? Yes or no?")
-    #         if password_again.lower() == "yes":
-    #             login = True
-    #         if password_again.lower() == "no": 
-    #             quit()
-            
-    # else:
-    #     username_again = input("Username incorrect. Would you like to try again? Yes or no?")
-    #     if username_again.lower() == "yes":
-    #         login = True
-    #     if username_again.lower() == "no":
-    #         print("Thank you for trying, welcome back later!")
-    #         quit()                                                                                                                      
-
