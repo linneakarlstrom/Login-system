cc785249d5f8753906049fd77c353abc633735e2
Linnea Karlström <linnea.karlstrom@skola.botkyrka.se>
2020-01-09 11:47:10 +0100

    work in progress

--- login_system.py
+++ login_system.py
@@ -19,16 +19,18 @@ users = [
 def login(username, password):
     for u in users:
         if u["username"] == username and u["password"] == password:
-            return True
-        
+            return True   
     return False
 
     # return username == users[0+1] password == users[0+1+1]
     # return username == "ICYLGR01" and password == "Cocoban123"   # det ska läggas in från dictionaryn och sedan kunna användas
     # om username är ICYLGR01 och lösenordet är Cocoban123 returnera True, annars returnera False
 
-def register():
-    return
+def register(username,password):
+    username = input("Write an username:")
+    password = input("Write a password:")
+    user_dictionary = {"username": username, "password": password}
+    return users.append(user_dictionary)
 
 login_in = True
 while login_in:
@@ -39,6 +41,12 @@ while login_in:
          quiz.start()  
          # quizet startas
     else:
-        print("Incorrect, you may try again.")
-        login_in = True
-        # startas om
+        tryagain_register = input("Incorrect, you want to try again? Yes or no?")
+        if tryagain_register == "yes".lower():
+            login_in = True
+         # startas om
+        
+        if tryagain_register == "no".lower():
+            register(username,password)
+            quiz.start() 
+        # användarnamn och lösenord registreras
--- quiz.py
+++ quiz.py
@@ -5,21 +5,21 @@ def start():
     
     questions = [ 
     ("Imagine you're in a dark room that is empty with nothing in it. There are no windows or doors. What is the easiest way to escape? \
-    \nA. Stop imagining that you are in a dark room.\nB. Die of course.\nC. Wait.\nD. Dig a hole in the ground with your bare hands.\nAnswer:", "A", "a", "Stop imagining you are in a dark room", "stop imagining you are in a dark room", "A. Stop imagining you are in a dark room"),
+    \nA. Stop imagining that you are in a dark room.\nB. Die of course.\nC. Wait.\nD. Dig a hole in the ground with your bare hands.\nAnswer:", "A".lower(), "Stop imagining you are in a dark room".lower(), "A. Stop imagining you are in a dark room".lower()),
     ("As the Titanic headed out to sea, what did the Captain say to the first officer?\nA.'Take her to sea Mr. Murdock, lets stretch her legs'. \
     \nB.'Get away from the wheel you jerk, I'm driving'.\nC.'Pleeeeeeeeeeease don't make me go. Somtin bad goin to happen'.\nD.'My boat, tee hee, my boat, tee hee, my boat, tee hee, can I ring the bell, tee hee.' \
     \nAnswer:", "A", "a", "'Take her to sea Mr. Murdock, lets stretch her legs'", "take her to sea Mr. Murdock, lets stretch her legs", "A. 'Take her to sea Mr. Murdock, lets stretch her legs'"),
     ("If you look you cannot see me. If you can see me you cannot see anything else.\nI can make anything you want to happen, but later on everything will go back to normal. What am I?\
-    \nA. Air.\nB. Nothing,\nC. Imagination.\nD. Other.\nAnswer:", "C", "c", "Imagination", "imagination", "C. Imagination" ),
+    \nA. Air.\nB. Nothing,\nC. Imagination.\nD. Other.\nAnswer:", "C".lower(), "Imagination", "imagination", "C. Imagination" ),
     ("The titanic was powered by\nA. Thousands of hamsters' running inside little wheels. \nB. The third class passengers rowing \nC. Sixteen giant steam boilers \
-    \nD. The crew members 'hocking lugies off the sterm all at once\nAnswer:", "C", "c", "Sixteen giant steam boilers", "sixteen giant steam boilers","C. Sixteen giant steam boilers"),
+    \nD. The crew members 'hocking lugies off the sterm all at once\nAnswer:", "C".lower(), "Sixteen giant steam boilers", "sixteen giant steam boilers","C. Sixteen giant steam boilers"),
     ("If your car begins to hydroplane (Hydroplaning means that water separates the tires from the ground and causes it to lose traction. You feel out of control.) you should:\
     \nA. Reduce your speed and let the car decelerate.\nB. Pump the breaks repeatedly.\nC. Immediatly slam the brakes.\
-    \nD. Do nothing and allow your car to turn into the plane it has always dreamed of.\nAnswer:", "D", "d", "Do nothing and allow your car to turn into the plane it has always dreamed of.", "do nothing and allow your car to turn into the plane it has always dreamed of.", \
-    "D. Do nothing and allow your car to turn into the plane it has always dreamed of."),
-    ("When was Albert Einstein born? \nA.In Switzerland \nB.In Germany \nC.In the United States \nD.In Israel.\nE.None of the above. \nAnswer:", "E", "e", "None of the above", "none of the above", "E.None of the above"),
-    ("There are 30 cows in a field, and 28 chickens. How many didn't?", "10", "ten", "Ten", "TEN", "10 didn't"),
-    ("Mental break time. This is the last question of the quiz. You deserve an easy question. What is 1 + 1?\nA. Not this one. \nB. Still not this one.\nC. Two\nD. You've gone too far, go back to C.\nAnswer:", "C", "c", "Two", "two", "C. 2", "2")
+    \nD. Do nothing and allow your car to turn into the plane it has always dreamed of.\nAnswer:", "D".lower(), "Do nothing and allow your car to turn into the plane it has always dreamed of.", "do nothing and allow your car to turn into the plane it has always dreamed of.", \
+    "D. Do nothing and allow your car to turn into the plane it has always dreamed of.", "C".lower(), "C. Immediatly slam the brakes.", "Immediatly slam the brakes.".lower())
+    ("When was Albert Einstein born? \nA.In Switzerland \nB.In Germany \nC.In the United States \nD.In Israel.\nE.None of the above. \nAnswer:", "E".lower(), "None of the above".lower(), "E.None of the above".lower()),
+    ("There are 30 cows in a field, and 28 chickens. How many didn't?", "10","Ten".lower(), "TEN", "10 didn't"),
+    ("Mental break time. This is the last question of the quiz. You deserve an easy question. What is 1 + 1?\nA. Not this one. \nB. Still not this one.\nC. Two\nD. You've gone too far, go back to C.\nAnswer:", "C".lower(), "Two".lower(), "C. 2".lower(), "2")
 ]
 
     for q in questions:
