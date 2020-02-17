e6e6012e868ecafdac6b943d76ac1b05f1803ba0
Linnea Karlström <linnea.karlstrom@skola.botkyrka.se>
2020-02-17 17:34:12 +0100

    problem have to be fixed

--- csvfiletest.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('data.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["SN", "Username", "Password"])
-    writer.writerow(["1", "ICYLGR01", "Cocoban123"])
-    writer.writerow(["2", "skalman", "fruktsallad"])
--- login_system.py
+++ login_system.py
@@ -1,54 +1,59 @@
 import quiz
-
-users = [
-    {
-        "username": "ICYLGR01",
-        "password": "Cocoban123", 
-    },
-    {
-        "username": "skalman",
-        "password": "fruktsallad"
-    }
-]
+import csv
+login_in = True
+# Programmet ska ej läsa in från listan utan från csv-filen
+#  users = [
+#     {
+#         "username": "ICYLGR01",
+#         "password": "Cocoban123", 
+#     },
+#     {
+#         "username": "skalman",
+#         "password": "fruktsallad"
+#     }
+# ]
 
 # def register(username,password):
 #     username = input("Username:")
 #     password = input("Password:")
 #     return # de nya keyvalue paren läggs in i users
 
-def login(username, password):
-    for u in users:
-        if u["username"] == username and u["password"] == password:
-            return True   
-    return False
+def login(logusername, logpassword):
+    with open('data.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            return logusername and logpassword
 
     # return username == users[0+1] password == users[0+1+1]
     # return username == "ICYLGR01" and password == "Cocoban123"   # det ska läggas in från dictionaryn och sedan kunna användas
     # om username är ICYLGR01 och lösenordet är Cocoban123 returnera True, annars returnera False
 
 def register(username, password):
-    user_dictionary = {"username": username, "password": password}
-    return users.append(user_dictionary)
-    import csv
-    with open('data.csv', 'w', newline='') as file:
-        writer = csv.writer(file)    
+    username = input("Your username: \n")
+    password = input("Your password \n")
+    user_list = [[username, password],
+                [" "]]
+    
+    with open('data.csv', 'a', newline='') as file:
+        writer = csv.writer(file) 
+        return  writer.writerows(user_list)   
 
-login_in = True
+
 while login_in:
-    username = input("Enter your username:")
-    password = input("Enter your password:")
+    username = input("Enter your username: \n")
+    password = input("Enter your password: \n")
     if login(username, password):
          print("Welcome", username, ", You may now progress to the quiz \n") # du lyckades
          quiz.start()  
          # quizet startas
     else:
-        tryagain = input("Incorrect, you want to try again? Yes or no?")
+        tryagain = input("Incorrect, you want to try again? Yes or no? \n")
         if tryagain == "yes".lower():
             login_in = True
          # startas om
         
         if tryagain == "no".lower():
-            not_login = input("Do you want to register and continue to the quiz, only register or none of the above?")
+            not_login = input("Do you want to register and continue to the quiz, only register or none of the above? \n ")
             if not_login == "Register and continue to the quiz".lower():
                 register(username, password)
                 quiz.start() 
