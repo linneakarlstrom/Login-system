55bfe1892039a6bf868caed79781d42dabf13659
Linnea Karlström <linnea.karlstrom@skola.botkyrka.se>
2020-02-19 21:04:01 +0100

    almost done

--- login_system.py
+++ login_system.py
@@ -15,48 +15,59 @@ import csv
 # def register(username,password):
 #     username = input("Username:")
 #     password = input("Password:")
-#     return # de nya keyvalue paren läggs in i users
-
-def login(logusername, logpassword):
-    logusername = input("Enter your username: \n")
-    logpassword = input("Enter your password: \n")
-    with open('data.csv', 'r') as file:
-        reader = csv.reader(file)
+#     return # de nya keyvalue paren läggs in i users)
 
     # return username == users[0+1] password == users[0+1+1]
     # return username == "ICYLGR01" and password == "Cocoban123"   # det ska läggas in från dictionaryn och sedan kunna användas
     # om username är ICYLGR01 och lösenordet är Cocoban123 returnera True, annars returnera False
 
-def register(username, password):
-    username = input("Your username: \n")
-    password = input("Your password \n")
-    user_list = [[username, password],
-                [" "]]
-    
-    with open('data.csv', 'a', newline='') as file:
-        writer = csv.writer(file) 
-        return  writer.writerows(user_list)   
-
-login_in = True
-while login_in:
-    if login(logusername, logpassword):
-        print("Welcome", logusername, ", You may now progress to the quiz \n") # du lyckades
-        quiz.start()  
-         # quizet startas
+def create_user(username,password):
+    with open('data.csv', 'r') as file:
+        reader = csv.reader(file)  
+    if :
+        print("found")
     else:
-        tryagain = input("Incorrect, you want to try again? Yes or no? \n")
-        if tryagain == "yes".lower():
-            login_in = True
-         # startas om
+        with open('data.csv', 'a', newline='') as file:
+        writer = csv.writer(file) 
+        user_list = [[username, password],
+                [" "]]
+        return writer.writerows(user_list)
         
-        if tryagain == "no".lower():
-            not_login = input("Do you want to register and continue to the quiz, only register or none of the above? \n ")
-            if not_login == "Register and continue to the quiz".lower():
-                register(username, password)
-                quiz.start() 
-            if not_login == "Only register".lower():
-                register(username, password)
-                break
-            if not_login == "None of the above".lower():
-                print("Goodbye! Hope to see you soon")
-                break
+    
+   
+
+def register():
+    username = input("Enter your username: \n")
+    password = input("Enter your password: \n")
+    create_user(username, password)
+    
+
+    
+register()
+    
+
+# login_in = True
+# while login_in:
+#     username = input("Enter your username: \n")
+#     password = input("Enter your password: \n")
+#     if register():
+#         print("Welcome", username, ", You may now progress to the quiz \n") # du lyckades
+#         quiz.start()  
+#          # quizet startas
+#     else:
+#         tryagain = input("Incorrect, you want to try again? Yes or no? \n")
+#         if tryagain == "yes".lower():
+#             login_in = True
+#          # startas om
+        
+#         if tryagain == "no".lower():
+#             not_login = input("Do you want to register and continue to the quiz, only register or none of the above? \n ")
+#             if not_login == "Register and continue to the quiz".lower():
+#                 create_user(username, password)
+#                 quiz.start() 
+#             if not_login == "Only register".lower():
+#                 create_user(username, password)
+#                 break
+#             if not_login == "None of the above".lower() or not_login == "no".lower():
+#                 print("Goodbye! Hope to see you soon")
+#                 break
